fbdfb1652e4e729a05d450a31aae8adc7ddd5dac
scott-hurtado <hurtadoscott3@gmail.com>
2026-08-04 19:20:55 -0700

    actualización del proyecto

--- main.go
+++ main.go
@@ -8,18 +8,10 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-// loadTemplates arma un único *template.Template con las páginas que viven
-// directo en templates/ (index.html, y luego blog.html, eccomerce.html...)
-// más los partials (templates/partials/*.html, como navbar.html) para que
-// {{template "navbar" .}} funcione dentro de cualquier página.
-//
-// Si ya tienes páginas de admin en templates/admin/*.html y también quieres
-// cargarlas con este mismo Engine, agrega aquí:
-//
-//	tmpl = template.Must(tmpl.ParseGlob("templates/admin/*.html"))
 func loadTemplates() *template.Template {
 	tmpl := template.Must(template.ParseGlob("templates/*.html"))
 	tmpl = template.Must(tmpl.ParseGlob("templates/partials/*.html"))
+	tmpl = template.Must(tmpl.ParseGlob("templates/auth/*.html"))
 	return tmpl
 }
 
@@ -28,6 +20,8 @@ func main() {
 
 	router.SetHTMLTemplate(loadTemplates())
 	router.Static("/static", "./static")
+	router.Static("/css", "./static/css")
+	router.Static("/js", "./static/js")
 
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "index.html", gin.H{
@@ -35,16 +29,17 @@ func main() {
 		})
 	})
 
-	// Cuando existan tienda.html / blog.html directo en templates/, se
-	// agregan aquí de la misma forma, pasando el ActivePage que
-	// corresponda para que el navbar marque el link activo:
-	//
-	// router.GET("/tienda", func(c *gin.Context) {
-	// 	c.HTML(http.StatusOK, "eccomerce.html", gin.H{"ActivePage": "tienda"})
-	// })
-	// router.GET("/blog", func(c *gin.Context) {
-	// 	c.HTML(http.StatusOK, "blog.html", gin.H{"ActivePage": "blog"})
-	// })
+	router.GET("/iniciar-sesion", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "iniciar-sesion.html", gin.H{
+			"ActivePage": "iniciar-sesion",
+		})
+	})
+
+	router.GET("/registro", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "registro.html", gin.H{
+			"ActivePage": "registro",
+		})
+	})
 
 	log.Println("Servidor corriendo en http://localhost:8080")
 	router.Run(":8080")
